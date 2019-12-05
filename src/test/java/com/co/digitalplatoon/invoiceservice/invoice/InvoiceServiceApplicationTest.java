a75766940fa36b9ee098779891b45f8c035912cc
F5160510 <koboti.mogale@fnb.co.za>
2019-12-05 14:49:11 +0200

    Added new project setup files

---
+++ src/test/java/com/co/digitalplatoon/invoiceservice/invoice/InvoiceServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package com.co.digitalplatoon.invoiceservice.invoice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class InvoiceServiceApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
